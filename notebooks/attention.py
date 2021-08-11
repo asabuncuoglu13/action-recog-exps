97b09919f68a9bc4ab34bd3c906f384541d4fbf8
alpay sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2021-08-11 16:40:31 +0300

    sasa-presentation

--- notebooks/attention.py
+++ notebooks/attention.py
@@ -5,6 +5,69 @@ import torch.nn.init as init
 
 import math
 
+class AttentionConv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, bias=False):
+        super(AttentionConv3d, self).__init__()
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.groups = groups
+
+        assert self.out_channels % self.groups == 0, "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
+
+        self.rel_h = nn.Parameter(torch.randn(out_channels // 2, 1, 1, kernel_size, 1), requires_grad=True)
+        self.rel_w = nn.Parameter(torch.randn(out_channels // 2, 1, 1, 1, kernel_size), requires_grad=True)
+
+        self.key_conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, bias=bias)
+        self.query_conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, bias=bias)
+        self.value_conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, bias=bias)
+
+        self.reset_parameters()
+
+    def forward(self, x):
+        batch, channels, height, width, d = x.size()
+        print("x size:", x.size())
+
+        padded_x = F.pad(x, (self.padding, self.padding, self.padding, self.padding, self.padding, self.padding))
+        print("padded x size:", padded_x.size())
+        
+        q_out = self.query_conv(x)
+        print("q_out:", q_out.size())
+        
+        k_out = self.key_conv(padded_x)
+        print("k_out:", k_out.shape)
+        
+        v_out = self.value_conv(padded_x)
+        print(v_out.size())
+        
+        k_out = k_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride).unfold(4, self.kernel_size, self.stride)
+        print("k_out:", k_out.shape)
+        
+        v_out = v_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride).unfold(4, self.kernel_size, self.stride)
+        print("v_out:", k_out.shape)
+        
+        print("rel_h:", self.rel_h.shape)
+        print("rel_w:", self.rel_w.shape)
+        
+        k_out = k_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, d, -1)
+        v_out = v_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, d, -1)
+
+        q_out = q_out.view(batch, self.groups, self.out_channels // self.groups, height, width, d, 1)
+
+        out = q_out * k_out
+        out = F.softmax(out, dim=-1)
+        out = torch.einsum('bncdhwk,bncdhwk -> bncdhw', out, v_out).view(batch, -1, height, width, d)
+        
+        return out
+
+    def reset_parameters(self):
+        init.kaiming_normal_(self.key_conv.weight, mode='fan_out', nonlinearity='relu')
+        init.kaiming_normal_(self.value_conv.weight, mode='fan_out', nonlinearity='relu')
+        init.kaiming_normal_(self.query_conv.weight, mode='fan_out', nonlinearity='relu')
+
+        init.normal_(self.rel_h, 0, 1)
+        init.normal_(self.rel_w, 0, 1)
 
 class AttentionConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, bias=False):
